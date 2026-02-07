acf8d2c326414f2cb5f5c4a614f74f922c9cb542
kingkobe20 <wamaithaforex@gmail.com>
2026-02-07 22:21:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,4 +21,6 @@ else:
 print("\nRecord saved successfully.")
 # Save record to file
 with open("records.txt", "a") as file:  # "a" = append
-    file.write(f"Sales: {sales}, Expenses: {expenses}, Profit: {profit}\n")
+    file.write(f"Sales: {sales}, 
+    Expenses: {expenses}, 
+    Profit: {profit}\n")
